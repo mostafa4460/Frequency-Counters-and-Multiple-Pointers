87b56dbb7d7707d0117733bc60550dc9e76ec0ff
Mostafa Nassr <mostafa.nassr88@gmail.com>
2021-06-24 00:33:43 -0400

    separatePositive

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,22 @@
-// add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(nums) {
+    // make 2 pointers that start from beginning: i & j
+    // while j is less than the length of nums
+        // if nums[i] is positive, increment i
+        // else if nums[j] is positive and nums[i] is negative, swap and increment i
+        // increment j
+    // return nums
+
+    let i = 0,
+        j = 0;
+    while (j < nums.length) {
+        if (nums[i] > 0) i += 1;
+        else if (nums[i] < 0 && nums[j] > 0) {
+            let temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+            i += 1;
+        }
+        j += 1;
+    }
+    return nums;
 }
