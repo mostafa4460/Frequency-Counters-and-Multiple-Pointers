0d71a14d3f9d4f9ee4a28cff1cd3ca042ae04f3b
Mostafa Nassr <mostafa.nassr88@gmail.com>
2021-06-23 23:08:40 -0400

    twoArrayObject

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,15 @@
-// add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, values) {
+    // make var obj = {}
+    // loop over keys array from start
+    // check if the indx exists in the values array or if it is undefined
+        // if exists, set key: value in obj
+        // else, set key: null in obj
+    // return obj
+
+    let obj = {};
+    for (let i = 0; i < keys.length; i++) {
+        if (values[i] === undefined) obj[keys[i]] = null;
+        else obj[keys[i]] = values[i];
+    }
+    return obj;
 }
